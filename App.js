3c616d70676be2805c454aa3e99ac30de8c4a419
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-14 16:13:45 -0300

    Se agregó la sección del login

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Hello</Text>
+      <Text style={styles.subTitle}>Sign In to your account</Text>
+      <TextInput
+        placeholder='jhon@email.com'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='password'
+        style={styles.textInput}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo: {
+    fontSize: 80,
+    color: '#34434D',
+    fontWeight: 'bold',
+  },
+
+  subTitle: {
+    fontSize: 20,
+    color: 'gray',
+
+  },
+
+  textInput: {
+    padding: 10,
+    paddingStart: 20,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  }
 });
